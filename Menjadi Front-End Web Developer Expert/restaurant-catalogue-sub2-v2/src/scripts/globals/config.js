ba212997bec7e776108f5a470441c81e75588d6d
SirajHammam <walfaridsi@gmail.com>
2022-11-28 14:04:33 +0700

    sub2

---
+++ Menjadi Front-End Web Developer Expert/restaurant-catalogue-sub2-v2/src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  KEY: 'YOUR_API_KEY',
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+};
+
+export default CONFIG;
